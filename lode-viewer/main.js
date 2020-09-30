e7469b9089796eaafff83a2eae0602215726c3ac
Robert Oikle <robert.oikle@canada.ca>
2020-09-30 08:51:31 -0400

    Added comments to main.js code

--- lode-viewer/main.js
+++ lode-viewer/main.js
@@ -24,19 +24,24 @@ function Start(results) {
 	var p1 = Promise.all(defs).then(values => {
 		config.maps = {};
 		
+		// Store map configurations in config object
 		values.forEach(v => config.maps[v.result.id] = Configuration.FromJSON(v.result));
 	});
 	
 	var p2 = Net.JSON(`${Core.root}config/config.bookmarks.json`).then(value => {
+		// Store bookmarks for CMA extents from bookmarks config file
 		config.bookmarks = value.result.items;
 	});
 	
 	var p3 = Net.JSON(`${Core.root}config/config.search.json`).then(value => {
+		// Store search CSD values from search config file
 		config.search = value.result;
 	});
 		
 	Promise.all([p1, p2, p3]).then(results => {
+		// Update app-container with application
 		var node = Dom.Node(document.body, "#app-container");
 		var app = new Application(node, config);
 	});
 }
+
