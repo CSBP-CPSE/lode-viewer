f97a2590c5b9b33d955b2534207eaf012edc12eb
Robert Oikle <robert.oikle@canada.ca>
2021-06-08 12:24:26 -0400

    Added documentation to table script

--- scripts/table.py
+++ scripts/table.py
@@ -14,10 +14,13 @@ INDEX = "Index"
 LAT = "Latitude"
 LONG = "Longitude"
 
+
 def BuildDf(iFile):
-    '''
+    """
     Build a Data Frame with the provided dataset
-    '''
+    :param iFile {string} - Path to input dataset
+    :return pandas dataframe
+    """
     df = pd.read_csv(iFile, index_col=False, encoding='utf-8', dtype={CSDUID: str})
 
     # Exclude null lat/long series from data frame
@@ -27,10 +30,11 @@ def BuildDf(iFile):
 
 
 def CreateTableConfig(path, config):
-    '''
-    Create the table config file and stores it in 
-    ./output/config/tables directory
-    '''
+    """
+    Create the table config file and stores it in ./output/config/tables directory
+    :param path {string} -
+    :param config {dictionary} - table configuration used to generate tables
+    """
     jPath = PurePath("./data", path)
     cPath = PurePath("./output", "./config", "./tables")
 
@@ -46,6 +50,13 @@ def CreateTableConfig(path, config):
 
 
 def CreateTableFiles(df, path, drop, config):
+    """
+    Create table files from data, and also the associated table config file
+    :param df {dataframe} - dataframe containing input data
+    :param path {string} - The ID of data being processed which is used to specify the output of the table files
+    :param drop {list} - a list of drop columns which were not used included in the output
+    :param config {dictionary} - configuration of how to generate the table output files
+    """
     # Create the string for the path for storing output data.
     tPath = PurePath("./output", "./data", path)
 
@@ -53,24 +64,34 @@ def CreateTableFiles(df, path, drop, config):
     Path(tPath).mkdir(parents=True, exist_ok=True)
 
     # Split dataframe content by CSD Unique ID
-    split = [pd.DataFrame(y) for x, y in df.groupby(CSDUID)]
+    csduid_groups = df.groupby(CSDUID)
+
+    # Loops through each csduid (x) in the groups and copies the associated csduid df (y) into the split list
+    split = [pd.DataFrame(y) for x, y in csduid_groups]
 
     config["summary"] = {}
 
+    # Loop through split dataframe content and output it's content as csv files
+    # Note: CSV files represent features in specific CSD's and are grouped by 50 records
     for s in split:
         CSD = str(s[CSDUID].iloc[0])
         s = s.sort_values(INDEX, ascending=True).drop(columns=drop)
+
+        # Break-up split CSD content into groups of 50 for each page of the table view
+        # Note: This commonly occurs for higher density urban areas.
         split_50 = [s[i:i + 49] for i in range(0, len(s), 50)]
 
         if len(s) > 0:
             config["summary"][CSD] = len(split_50)
 
         for index, s_50 in enumerate(split_50):
+            # Update output file name based on index of the 50 record group
             oFile = CSD + '_' + str(index + 1) + '.json'
 
             # Output CSV file with json extension because NDM server don't allow CSV files for some reason.
             u.DumpCSV(tPath.joinpath(oFile), s_50)
 
+    # Create a table config file
     CreateTableConfig(path, config)
 
 
@@ -89,11 +110,13 @@ def CreateShapefile(df, oFile, drop):
 # Base table configuration, this needs to be changed by dataset
 config = u.ReadJSON(TABLECONFIG)
 
+# Create a dataframe based on data source URL provided in the config file
 df = BuildDf(config["source"])
 
 # Create a list of dropped fields not listed in the table config file
 drop = u.GetDropFields(df, config["fields"])
 
+# Create table files for data based on boundary locations
 CreateTableFiles(df, config["id"], drop, config)
 
 # CreateShapefile(df, config["id"] + ".shp", drop)
